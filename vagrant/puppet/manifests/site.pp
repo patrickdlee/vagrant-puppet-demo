6cc6994e4565184fcb5307178f60cafe4184f38e
Patrick Lee <patrick.lee@bodybuilding.com>
2014-04-29 16:03:49 -0600

    added some comments in Puppet code

--- vagrant/puppet/manifests/site.pp
+++ vagrant/puppet/manifests/site.pp
@@ -1,15 +1,18 @@
-# instancerole is required
+# instancerole fact is required
 if $::instancerole == undef {
   fail('Missing instancerole fact.')
 }
 
+# add new run stage that precedes main stage
 stage { 'pre': before => Stage['main'] }
 
+# make sure baseconfig is applied first
 class { 'baseconfig':
   stage => 'pre',
   user  => $::ssh_username
 }
 
+# set defaults for File resources
 File {
   ensure => file,
   owner  => 'root',
@@ -17,6 +20,7 @@ File {
   mode   => '0644'
 }
 
+# determine role from instancerole fact
 case $::instancerole {
   appserver: {
     include apache, mysql, php, appserver
--- vagrant/puppet/modules/mysql/manifests/init.pp
+++ vagrant/puppet/modules/mysql/manifests/init.pp
@@ -10,6 +10,8 @@ class mysql {
     require => Package['mysql-server'];
   }
 
+  # NOTE: I do NOT recommend using Puppet to set your MySQL root password. This
+  #   is just to demonstrate an 'exec' resource and get up and running faster.
   exec { 'set-mysql-password':
     unless  => 'mysqladmin -uroot -proot status',
     command => 'mysqladmin -uroot password root',
